e746b2eb802559778de675578260079b4b7ef758
LostoVayne <moises.barrera.dev@gmail.com>
2023-07-03 10:32:07 -0400

    fix

--- src/components/Sidebar/Lenguage.jsx
+++ src/components/Sidebar/Lenguage.jsx
@@ -1,4 +1,4 @@
 const Lenguage = ({ children }) => {
-    return <div className='flex items-center gap-2'>{children}</div>
+  return <div className='flex items-center gap-2'>{children}</div>
 }
 export default Lenguage
